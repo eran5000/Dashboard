a143f20af604b66303873cdc5361048f9c95ee15
Tal Liber <tal.liber@gmail.com>
2023-08-17 19:00:46 +0300

    charts fixed

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,10 +4,10 @@
     <AppHeader />
     <section class="content-container">
       <section class="charts-container" v-if="chartsData">
-        <Charts label="Payments" :dataSet="[1857, 716, 9048, 6443, 5782]" :labels="chartsData.dates" />
-        <Charts label="Searches" :dataSet="[57, 46, 340, 210, 330]" :labels="chartsData.dates" />
-        <Charts label="RPM" :dataSet="[34, 17, 29, 33, 20]" :labels="chartsData.dates" />
-        <Charts label="Alerts" :dataSet="[112, 587, 144, 372, 1968]" :labels="chartsData.dates" />
+        <Charts label="Payments" :dataSet="[1857, 716, 9048, 6443, 5782]" :labels="['25-03','24-03', '23-03', '22-03', '21-03']" />
+        <Charts label="Searches" :dataSet="[57, 46, 340, 210, 330]" :labels="['25-03','24-03', '23-03', '22-03', '21-03']" />
+        <Charts label="RPM" :dataSet="[34, 17, 29, 33, 20]" :labels="['25-03','24-03', '23-03', '22-03', '21-03']" />
+        <Charts label="Alerts" :dataSet="[112, 587, 144, 372, 1968]" :labels="['25-03','24-03', '23-03', '22-03', '21-03']" />
       </section>
       <DataTable @sort="sortEntities" :entities="entities" />
     </section>
