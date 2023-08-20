1c750f5ef019c3637a942c2cf868d2ad8957c4ed
Eran <peled2k@gmail.com>
2023-08-20 18:00:19 +0300

    please

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: '/vite-deploy-demo/'
+  base: '/vite-deploy-demo/'
 })
